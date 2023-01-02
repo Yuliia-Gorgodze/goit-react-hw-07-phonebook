f6a7cb3082fd91643849882ae3917338b2346da2
8Tanya28 <4090535@gmail.com>
2023-01-02 13:07:50 +0200

    fineness

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ class App extends Component {
   render() {
     return (
       <div>
-        <h1>Телефонная книга</h1>
+        <h1>ТЕЛЕФОННАЯ КНИГА</h1>
         <AddNameContact />
         <h2>Мои контакты: </h2>
         <Filter />
